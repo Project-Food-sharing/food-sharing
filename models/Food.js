babaea354b0160762c7f006f0b2fcb5f054a4a9f
Veronica De Leon <v.dleon@gmail.com>
2020-09-14 16:24:42 +0200

    day 1

---
+++ models/Food.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const celebritySchema = new Schema({
+  title: String,
+  description: String,
+  categories: {
+    type: [String],
+    enum: ["Raw", "Prepared", "Drinks"],
+  },
+  status: {
+    type: String,
+    enum: ["Available", "Blocked", "Gone"],
+    default: "Available",
+  },
+  // creator: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  // },
+  latitud: String,
+  longitud: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  // imgName: String,
+  // imgPath: String,
+  // imgPublicId: String,
+});
+
+const Food = mongoose.model("Food", celebritySchema);
+module.exports = Food;
