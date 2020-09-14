33009918db41d03900feb31c01b4ae8ed1704a53
Khushboo Goyal <khushboo026@gmail.com>
2020-09-14 16:28:33 +0200

    first day changes

--- app.js
+++ app.js
@@ -30,6 +30,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//session configuration
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {maxAge:60*60*1000},
+    saveUninitialized: false,
+    resave:true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60* 60 * 1000
+    })
+  })
+)
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -47,12 +64,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Food-Sharing';
 
 
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const user = require('./routes/user/user.js');
+app.use('/', user); 
 
 
 module.exports = app;
